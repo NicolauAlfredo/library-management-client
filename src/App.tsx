cd0c5f05842ef5610c32e24d4ea5bb09940e6c45
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-06-21 00:13:02 +0200

    feat: configure auth api client

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { RouterProvider } from "react-router-dom";
-import { Router } from "react-router-dom";
 import { router } from "./routes/app.routes";
 
 function App() {
---
+++ src/types/api.ts
@@ -0,0 +1,24 @@
+import { api } from "axios";
+
+import { AuthResponse, LoginRequest } from "../types/auth";
+import type { User } from "./user";
+
+interface ApiResponse<T> {
+  success: boolean;
+  data: T;
+}
+
+export async function loginResquest(data: LoginRequest): Promise<AuthResponse> {
+  const response = await api.post<ApiResponse<AuthResponse>>(
+    " /auth/login",
+    data,
+  );
+
+  return response.data.data;
+}
+
+export async function getProfieRequest(): Promise<User> {
+  const response = await api.get<ApiResponse<User>>("/auth/profile");
+
+  return response.data.data;
+}
