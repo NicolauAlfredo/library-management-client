c0423d75a45d9de40fe77f2b3103c936ce8ee9ca
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-06-21 00:46:39 +0200

    feat: add protected routes

---
+++ src/components/protected-route/ProtectedRoute.tsx
@@ -0,0 +1,16 @@
+import { Navigate, Outlet, replace } from "react-router-dom";
+import { useAuth } from "../../contexts/auth.context";
+
+export function ProtectedRoute() {
+  const { isAuthenticated, isLoading } = useAuth();
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return <Outlet />;
+}
--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,10 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 
+import { ProtectedRoute } from "../components/protected-route/ProtectedRoute";
+
 import { LoginPage } from "../pages/login/LoginPage";
 import { DashboardPage } from "../pages/dashboard/DashboardPage";
 import { BooksPage } from "../pages/books/BooksPage";
 import { UsersPage } from "../pages/users/UsersPage";
 import { LoansPage } from "../pages/loans/LoansPage";
+import path from "path";
 
 export const router = createBrowserRouter([
   {
@@ -12,19 +15,24 @@ export const router = createBrowserRouter([
     element: <LoginPage />,
   },
   {
-    path: "/",
-    element: <DashboardPage />,
-  },
-  {
-    path: "/books",
-    element: <BooksPage />,
-  },
-  {
-    path: "/users",
-    element: <UsersPage />,
-  },
-  {
-    path: "/loans",
-    element: <LoansPage />,
+    element: <ProtectedRoute />,
+    children: [
+      {
+        path: "/",
+        element: <DashboardPage />,
+      },
+      {
+        path: "/books",
+        element: <BooksPage />,
+      },
+      {
+        path: "/users",
+        element: <UsersPage />,
+      },
+      {
+        path: "/loans",
+        element: <LoansPage />,
+      },
+    ],
   },
 ]);
