3d20d4aff4dcf6778276b92d46898c401525792c
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-06-21 10:36:11 +0200

    feat: fetch and manage users

--- src/types/user.ts
+++ src/types/user.ts
@@ -5,6 +5,20 @@ export interface User {
   name: string;
   email: string;
   role: Role;
+  deletedAt: string | null;
   createdAt: string;
   updatedAt: string;
+}
+
+export interface UserFilters {
+  page?: number;
+  limit?: number;
+  search?: string;
+  role?: Role;
+}
+
+export interface UpdateUserData {
+  name?: string;
+  email?: string;
+  role?: Role;
 }
